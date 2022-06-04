07815d7d8cacde2934fcd2f3ddcc31bce994c5a7
matheus <mcbattirola@hotmail.com>
2022-06-04 14:00:23 -0300

    Bootstrap tests for cli and gn

---
+++ pkg/cli/cli_test.go
@@ -0,0 +1,8 @@
+package cli
+
+import "testing"
+
+func testRun(t *testing.T) {
+	// test that it requires a subcommand
+	// validate inputs (branch is required if project is set, etc)
+}
---
+++ pkg/gn/gn_test.go
@@ -0,0 +1,31 @@
+package gn
+
+import (
+	"testing"
+)
+
+func testGetProjectRoot(t *testing.T) {
+	// create temp dir
+	// projName := t.TempDir()
+
+	// start a git repo in it
+	// path, err := exec.Command("git", "rev-parse", "--show-toplevel").Output()
+
+	// expect the value returned to be projName
+
+	// also, test running in a path that is not a git repo returns an error
+}
+
+func testGetCurrentBranch(t *testing.T) {
+	// create temp dir and branch
+	// projName := t.TempDir()
+	// git init into it
+	// create branch
+
+	// get current branch
+	// expect it to be right
+
+	// change branch and see if it changes too
+
+	// test that it returns an error if run in not a git dir
+}
