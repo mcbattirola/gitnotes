c6ef9765e96bde6ab5bdd84a4471c0283061de51
Matheus <mcbattirola@hotmail.com>
2022-07-19 09:37:44 -0300

    Remove empty test

--- pkg/gn/gn_test.go
+++ pkg/gn/gn_test.go
@@ -8,14 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// We'll need some mocks to properly test this
-func TestEdit(t *testing.T) {
-	// assert that it commits if AlwaysCommit is true
-	// assert that it uses the current project if none is provided
-	// assert that it uses the current branch if none is provided
-	// assert that it uses the received project and branch
-}
-
 func TestFindProject(t *testing.T) {
 	if os.Getenv("GN_TEST_INTEAGRATION") != "TRUE" {
 		t.Skip("skipping integration test TestFindProject")
